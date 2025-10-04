08abb461dd1ca003273d9c31d8fdbf77721299d1
RAGUNATH R <113915622+Ragu-123@users.noreply.github.com>
2025-10-04 15:16:28 +0530

    Delete check.py

--- check.py
+++
@@ -1,5 +0,0 @@
-from transformers import AutoTokenizer
-
-tok = AutoTokenizer.from_pretrained("HuggingFaceTB/SmolLM2-360M-Instruct")
-print("EOS token:", tok.eos_token)
-print("EOS token id:", tok.eos_token_id)
